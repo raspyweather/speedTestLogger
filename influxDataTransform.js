40351b2d10e87b19afd72662f7cfd5799f2b405e
raspyweather <raspyweather@gmail.com>
2019-04-27 18:11:39 +0200

    typo corrected

--- influxDataTransform.js
+++ influxDataTransform.js
@@ -3,18 +3,16 @@ module.exports = {
         return [{
             measurement: measurementName,
             fields: {
-                fields: {
-                    success: data !== undefined,
-                    pingInMs: data !== undefined ? data.server.ping : -1,
-                    downInMbps: data !== undefined ? data.speeds.originalDownload : -1,
-                    upInMbps: data !== undefined ? data.speeds.originalUpload : -1,
-                    distance: data !== undefined ? data.server.distance : -1,
-                    serverId: data !== undefined ? data.server.id : -1,
-                },
-                tags: {
-                    lat: data !== undefined ? data.server.lat : -1,
-                    lon: data !== undefined ? data.server.lon : -1
-                }
+                success: data !== undefined,
+                pingInMs: data !== undefined ? data.server.ping : -1,
+                downInMbps: data !== undefined ? data.speeds.originalDownload : -1,
+                upInMbps: data !== undefined ? data.speeds.originalUpload : -1,
+                distance: data !== undefined ? data.server.distance : -1,
+                serverId: data !== undefined ? data.server.id : -1,
+            },
+            tags: {
+                lat: data !== undefined ? data.server.lat : -1,
+                lon: data !== undefined ? data.server.lon : -1
             }
         }];
     }
