b38060a48dabe78a233aa7cfc514a8be1868df39
raspyweather <raspyweather@gmail.com>
2020-02-04 12:58:31 +0100

    workaround no longer necessary

--- speedTest.js
+++ speedTest.js
@@ -2,7 +2,7 @@ const speedTest = require('speedtest-net');
 module.exports = {
     testSpeed: () =>
         new Promise((resolve, reject) => {
-            const test = speedTest({ maxTime: 30000, serversUrl: 'http://c.speedtest.net/speedtest-servers-static.php' });
+            const test = speedTest({ maxTime: 30000 });
             test.on('data', resolve);
             test.on('error', reject);
         })
