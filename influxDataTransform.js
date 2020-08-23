fbed32db013492a41ae8fe40a237e46384a3ec6a
Patrick Krämer <patagonahn@gmail.com>
2020-08-23 22:56:05 +0200

    null instead of -1 for missing values

--- influxDataTransform.js
+++ influxDataTransform.js
@@ -4,15 +4,15 @@ module.exports = {
             measurement: measurementName,
             fields: {
                 success: data != null,
-                ping: data != null ? data.server.ping : -1,
-                down: data != null ? data.speeds.originalDownload : -1,
-                up: data != null ? data.speeds.originalUpload : -1,
-                distance: data != null ? data.server.distance : -1,
-                lat: data != null ? data.server.lat : -1,
-                lon: data != null ? data.server.lon : -1
+                ping: data != null ? data.server.ping : null,
+                down: data != null ? data.speeds.originalDownload : null,
+                up: data != null ? data.speeds.originalUpload : null,
+                distance: data != null ? data.server.distance : null,
+                lat: data != null ? data.server.lat : null,
+                lon: data != null ? data.server.lon : null
             },
             tags: {
-                serverId: data != null ? data.server.id : -1,
+                serverId: data != null ? data.server.id : null,
                 isp: data != null ? data.client.isp : null
             }
         }];
